faf05f9f43026db59805b8fd0674b361609ef2ba
Jude Lawson <lawsonjdutton@gmail.com>
2018-03-31 23:06:45 -0600

    Adds todo comment, HTML output is WIP

--- server.rb
+++ server.rb
@@ -4,6 +4,8 @@ server = worker.create
 
 loop do
   connection = server.accept
+  # output needs to come from htmlparser class using erb
+  # instance variables can be passed to the output for request increments
   output = "<html><head></head></body><p>Hello, World! (0)</p></body></html>"
   connection.puts worker.response(:OK, "Content-Length: #{output.length}\r\n")
   connection.puts output
