e1b5a61d1bac0e88f70a2ef603e1dfd497f2b7ed
Jude Lawson <lawsonjdutton@gmail.com>
2018-03-31 22:39:27 -0600

    Adds in class attributes for status codes and method for header formatting

--- lib/server_worker.rb
+++ lib/server_worker.rb
@@ -3,10 +3,19 @@ require 'socket'
 class ServerWorker
   def initialize(port)
     @port = port
+    @status_codes = {
+      OK: "HTTP/1.1 200 OK\r\n"
+    }
   end
 
   def create
     TCPServer.new(@port)
   end
+
+  def response(status, headers)
+    "#{@status_codes[status]}#{headers}\r\n"
+  end
+
+
 
 end
