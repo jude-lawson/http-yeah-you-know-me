7b23979f76b40b5a678507db4e90133ebc767f12
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-02 17:16:13 -0600

    Adds in support for ServerWorker setup and teardown in tests

--- server.rb
+++ server.rb
@@ -1,12 +1,4 @@
 require_relative './lib/server_worker'
 worker = ServerWorker.new(9292)
 
-# loop do
-#   client = worker.create_client
-#   worker.receive_request(client)
-#   puts "Received request: "
-#   p worker.request
-#   worker.close(client)
-# end
-
 worker.start_server_loop
--- test/server_worker_test.rb
+++ test/server_worker_test.rb
@@ -7,16 +7,31 @@ require_relative '../lib/server_worker'
 
 class ServerWorkerTest < Minitest::Test
 
+  def setup
+    @worker = ServerWorker.new(3000)
+  end
+
+  def teardown
+    @worker.server.close
+  end
+
   def test_worker_and_server_exist
-    worker = ServerWorker.new(3000)
-    server = worker.server
-    assert_instance_of ServerWorker, worker
+    # worker = ServerWorker.new(3000)
+    server = @worker.server
+    assert_instance_of ServerWorker, @worker
     assert_instance_of TCPServer, server
+    # @worker.server.close
   end
 
   def test_server_responds
     res = Net::HTTP.get_response(URI('http://localhost:9292'))
     assert res
   end
+
+  def test_server_loop_can_start
+    # worker = ServerWorker.new(3000)
+    # @worker.server.close
+  end
+
 
 end
