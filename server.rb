2f90c1e50316b1da5b8add203162c27d293a7548
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-02 15:12:30 -0600

    Restarts work on ServerWorker class.

--- server.rb
+++ server.rb
@@ -1,13 +1,15 @@
 require_relative './lib/server_worker'
 worker = ServerWorker.new(9292)
-server = worker.create
+server = worker.server
 
-loop do
-  connection = server.accept
-  puts "Connected"
-  worker.successful_request
-  output = worker.output
-  connection.puts worker.response(:OK, "Content-Length: #{output.length}\r\n")
-  connection.puts output
-  connection.close
-end
+worker
+
+# loop do
+# connection = server.accept
+# puts "Connected"
+# worker.successful_request
+# output = worker.output
+# connection.puts worker.response(:OK, "Content-Length: #{output.length}\r\n")
+# connection.puts output
+# connection.close
+# end
--- test/server_worker_test.rb
+++ test/server_worker_test.rb
@@ -1,51 +1,19 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'net/http'
 require_relative '../lib/server_worker'
 
+# Start server to run tests
+
 class ServerWorkerTest < Minitest::Test
-  def setup
-    @pid = spawn("ruby ./server.rb")
-    # Process.wait @pid
-  end
 
-  def teardown
-    puts @pid
-    `kill -9 #{@pid}`
-    puts "A teardown"
-  end
-
-  def test_server_worker_exists
-    skip
+  def test_worker_exists
     worker = ServerWorker.new(9292)
     assert_instance_of ServerWorker, worker
   end
 
-  def test_server_worker_can_create_tcp_server
+  def test_server_exists
     skip
     worker = ServerWorker.new(9292)
-    server = worker.create
-    assert_instance_of TCPServer, server
-  end
-
-  
-  def test_server_responds_to_http_request_on_port_9292
-    res = Net::HTTP.get_response(URI('http://localhost:9292'))
-    assert res.body
-  end
-
-  def test_server_can_respond_with_hello_world_html
-    skip
-    res = Net::HTTP.get_response(URI('http://localhost:9292'))
-    expected = "<html><head></head></body><p>Hello, World! (2)</p></body></html>"
-    assert_equal expected, res.body
-  end
-  
-  def test_that_server_records_number_of_responses
-    skip
-    res = Net::HTTP.get_response(URI('http://localhost:9292'))
-    expected = "<html><head></head></body><p>Hello, World! (3)</p></body></html>"
-    assert_equal expected, res.body
   end
 
 end
