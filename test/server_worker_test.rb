b2a99b10701bc5ff8734715350d6caf7e9a75cb5
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-02 16:40:02 -0600

    Adds in test for server response, ready for a quick refactor

--- test/server_worker_test.rb
+++ test/server_worker_test.rb
@@ -1,19 +1,22 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'net/http'
 require_relative '../lib/server_worker'
 
-# Start server to run tests
+# =============== Start server to run tests =============
 
 class ServerWorkerTest < Minitest::Test
 
-  def test_worker_exists
-    worker = ServerWorker.new(9292)
+  def test_worker_and_server_exist
+    worker = ServerWorker.new(3000)
+    server = worker.server
     assert_instance_of ServerWorker, worker
+    assert_instance_of TCPServer, server
   end
 
-  def test_server_exists
-    skip
-    worker = ServerWorker.new(9292)
+  def test_server_responds
+    res = Net::HTTP.get_response(URI('http://localhost:9292'))
+    assert res
   end
 
 end
