06ca933d6f16feb950db3afdf8a211bb58ad7a83
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-03 11:50:46 -0600

    Pulls out response methods to Response class

--- lib/server_worker.rb
+++ lib/server_worker.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require_relative './router'
 
 class ServerWorker
   attr_reader :server,
@@ -7,14 +8,26 @@ class ServerWorker
   def initialize(port)
     @server = TCPServer.new(port)
     @request = []
+    @request_count = 0
   end
 
   def start_server_loop
     loop do
+      puts "Server has started!"
       client = create_client
       receive_request(client)
       puts "Received request: "
-      p request
+      p @request
+      route = Router.new(client)
+      route.respond('/')
+      # ======= #
+      # output = "<html><head></head><body><p>Hello, World! (#{@request_count})4</p></body></html>"
+      # headers = ["HTTP/1.1 200 ok\r\n",
+      #           "Content-Length: #{output.length}\r\n\r\n"]
+      # client.puts Response.headers
+      # client.puts Response.body
+      # client.puts headers
+      # client.puts output
       close(client)
     end
   end
@@ -30,6 +43,11 @@ class ServerWorker
       @request << line
       line = client.gets
     end
+    add_to_request_count
+  end
+
+  def add_to_request_count
+    @request_count += 1
   end
 
   def close(client)
