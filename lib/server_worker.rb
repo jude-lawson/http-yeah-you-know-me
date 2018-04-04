0cc060def9a05460229b170a54d57efead8e8602
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-04 14:27:06 -0600

    Updates server loop to methodize some behvaior

--- lib/server_worker.rb
+++ lib/server_worker.rb
@@ -7,45 +7,54 @@ class ServerWorker
 
   def initialize(port)
     @server = TCPServer.new(port)
+    @router = Router.new
     @request = []
     @request_count = 0
   end
 
   def start_server_loop
     loop do
+      # puts "Server has started!"
+      # client = create_client
+      # receive_request(client)
+      # puts "Received request: "
+      # p @request
+      # @router.respond('/')
+      # # ======= #
+      # # output = "<html><head></head><body><p>Hello, World! (#{@request_count})4</p></body></html>"
+      # # headers = ["HTTP/1.1 200 ok\r\n",
+      # #           "Content-Length: #{output.length}\r\n\r\n"]
+      # # client.puts Response.headers
+      # # client.puts Response.body
+      # # client.puts headers
+      # # client.puts output
+      # close(client)
       puts "Server has started!"
-      client = create_client
+      client = @server.accept
       receive_request(client)
-      puts "Received request: "
-      p @request
-      route = Router.new(client)
-      route.respond('/')
-      # ======= #
-      # output = "<html><head></head><body><p>Hello, World! (#{@request_count})4</p></body></html>"
-      # headers = ["HTTP/1.1 200 ok\r\n",
-      #           "Content-Length: #{output.length}\r\n\r\n"]
-      # client.puts Response.headers
-      # client.puts Response.body
-      # client.puts headers
-      # client.puts output
-      close(client)
+      request = handle_request
+      create_response(request)
+
     end
   end
 
-  def create_client
-    @server.accept
-  end
-
-  def receive_request(client)
-    @request = []
+  def handle_request(client)
+    lines__of_request = []
     line = client.gets
     until line == "\r\n"
-      @request << line
+      lines_of_request << line
       line = client.gets
     end
     add_to_request_count
+    Request.new(lines_of_request)
   end
 
+  def create_response(request)
+    
+    
+
+
+  # Needs a test
   def add_to_request_count
     @request_count += 1
   end
