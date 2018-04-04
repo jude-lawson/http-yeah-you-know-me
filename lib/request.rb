4f713cd85ad18aa3952a1b3288a8825444d1ba8d
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-04 16:06:18 -0600

    Passes all test but method, path, and protocol

--- lib/request.rb
+++ lib/request.rb
@@ -1,18 +1,23 @@
 class Request
+  attr_reader :initial_line,
+              :method,
+              :path,
+              :protocol,
+              :headers,
+              :body
 
   def initialize(lines_of_request)
-    # Method
-    # Path
-    # Version of protocol
-    # Headers hash
-    # Body string
+    @lines_of_request = lines_of_request
+    @initial_line = lines_of_request[0]
+    @method = nil
+    @path = nil
+    @protocol = nil
+    @headers = lines_of_request[1...-2]
+    @body = lines_of_request[-1]
   end
 
-  def parse_request
-    # Go through each line of request
-    # First is status
-    # Each after has key based on words before first :
-    # return a response object
+  def parse_initial_line
+    
   end
 
 end
