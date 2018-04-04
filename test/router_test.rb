b7ec4b240d344500c2a70ecd662b26c2d969a22b
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-04 15:34:09 -0600

    Removes uneeded tests

--- test/router_test.rb
+++ test/router_test.rb
@@ -1,18 +1,20 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative '../lib/router'
+require './lib/router'
+require './lib/response'
+require 'socket'
 
 class RouterTest < Minitest::Test
   def setup
-    @router = Router.new('/')
+    @router = Router.new
   end
 
   def test_router_exists
     assert_instance_of Router, @router
   end
 
-  def test_route_to_root_returns_hello_world
-    expected = "Hello World!"
-    assert_equal expected, @router.route
+  def test_route_to_root_returns_a_response_object
+    assert_instance_of Response, @router.respond('/')
   end
+
 end
