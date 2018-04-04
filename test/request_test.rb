8dc7209bbc0bf6481d5078c3d7ecec13f9ee0044
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-04 16:06:43 -0600

    Adds in initial_line test

--- test/request_test.rb
+++ test/request_test.rb
@@ -1,30 +1,22 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative '../lib/request'
+require './lib/request'
 
 class RequestTest < Minitest::Test
   def setup
-    lines_of_request = [
-      "GET /hello HTTP/1.1\r\n",
-      "Host: localhost:9292\r\n",
-      "Connection: keep-alive\r\n",
-      "Cache-Control: max-age=0\r\n",
-      "Upgrade-Insecure-Requests: 1\r\n",
-      "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36\r\n",
-      "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\r\n",
-      "Accept-Encoding: gzip, deflate, br\r\n",
-      "Accept-Language: en-US,en;q=0.9\r\n"
-      "\r\n",
-      "<html>
-        <head></head>
-        <body><p>Hello, World! (3)</p>
-      </html>"
-    ]
+    lines_of_request = ["GET /hello HTTP/1.1\r\n","Host: localhost:9292\r\n","Connection: keep-alive\r\n","Cache-Control: max-age=0\r\n","Upgrade-Insecure-Requests: 1\r\n","User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36\r\n","Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\r\n","Accept-Encoding: gzip, deflate, br\r\n","Accept-Language: en-US,en;q=0.9\r\n","\r\n","<html><head></head><body><p>Hello, World! (3)</p></html>"]
     @request = Request.new(lines_of_request)
+    @request.parse_initial_line
   end
 
   def test_request_exists
     assert_instance_of Request, @request
+  end
+
+  def test_request_initial_line
+    expected = "GET /hello HTTP/1.1\r\n"
+    actual = @request.initial_line
+    assert_equal expected, actual
   end
 
   def test_request_method
@@ -46,14 +38,14 @@ class RequestTest < Minitest::Test
   end
   
   def test_request_headers
-    expected = {host: "localhost:9292", accept_language: "eng"}
+    expected = ["Host: localhost:9292\r\n","Connection: keep-alive\r\n","Cache-Control: max-age=0\r\n","Upgrade-Insecure-Requests: 1\r\n","User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36\r\n","Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\r\n","Accept-Encoding: gzip, deflate, br\r\n","Accept-Language: en-US,en;q=0.9\r\n"]
     actual = @request.headers
     assert_equal expected, actual
   end
   
   def test_request_body
     expected = "<html><head></head><body><p>Hello, World! (3)</p></html>"
-    actual = @response.body
+    actual = @request.body
     assert_equal expected, actual
   end
 
