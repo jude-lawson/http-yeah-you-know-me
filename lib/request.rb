d57576b55ccafbc53b78d21a5f7e890190c28762
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-04 14:27:29 -0600

    Adds Request class

---
+++ lib/request.rb
@@ -0,0 +1,18 @@
+class Request
+
+  def initialize
+    # Method
+    # Path
+    # Version of protocol
+    # Headers hash
+    # Body string
+  end
+
+  def parse_request
+    # Go through each line of request
+    # First is status
+    # Each after has key based on words before first :
+    # return a response object
+  end
+
+end
