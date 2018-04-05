65aa3d5cb74f8887da1350ed68b41ef54056a678
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-04 20:50:47 -0600

    Updates test to accomodate added count parameter for request class

--- test/response_test.rb
+++ test/response_test.rb
@@ -7,7 +7,7 @@ require './lib/router'
 class ResponseTest < Minitest::Test
   def setup
     lines_of_request = ["GET /hello HTTP/1.1\r\n","Host: localhost:9292\r\n","Connection: keep-alive\r\n","Cache-Control: max-age=0\r\n","Upgrade-Insecure-Requests: 1\r\n","User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36\r\n","Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\r\n","Accept-Encoding: gzip, deflate, br\r\n","Accept-Language: en-US,en;q=0.9\r\n","\r\n","<html><head></head><body><p>Hello, World! (3)</p></html>"]
-    request = Request.new(lines_of_request)
+    request = Request.new(lines_of_request, 0)
     @response = Response.new(request)
   end
 
