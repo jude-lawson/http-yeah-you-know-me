b6807672649ae0d30a1aa6f06453c2a4be7daa68
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-04 19:14:18 -0600

    Sets up ResponseTest scaffold

--- test/response_test.rb
+++ test/response_test.rb
@@ -1,10 +1,13 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative '../lib/response'
+require './lib/response'
+require './lib/request'
 
 class ResponseTest < Minitest::Test
   def setup
-    @response = Response.new
+    lines_of_request = ["GET /hello HTTP/1.1\r\n","Host: localhost:9292\r\n","Connection: keep-alive\r\n","Cache-Control: max-age=0\r\n","Upgrade-Insecure-Requests: 1\r\n","User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36\r\n","Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\r\n","Accept-Encoding: gzip, deflate, br\r\n","Accept-Language: en-US,en;q=0.9\r\n","\r\n","<html><head></head><body><p>Hello, World! (3)</p></html>"]
+    request = Request.new(lines_of_request)
+    @response = Response.new(request)
   end
 
   def test_response_object_exists
